f8a4a3122dd4d17ea6c6b595c4261cc77b192f52
Daniel Mærsk Pedersen <pedersendaniel3561@gmail.com>
2024-11-15 12:32:07 +0100

    created some plots

--- main/main.py
+++ main/main.py
@@ -1,16 +1,19 @@
 import time
-from modules import LogdistancePathLossModel, Pipeline, RSSICollector, MeanFilter, CSVLogger
+from modules import *
 
 INTERVAL = 0.1  # If too low, the queue will fill up faster than the data can be processed
 
 pipeline = Pipeline()
 
 rssi_collector = RSSICollector(interval=INTERVAL)
-mean_filter = MeanFilter(window_size=20)
+#filter = MeanFilter(window_size=20)
+#filter = KalmanFilter(dt=INTERVAL, process_var=0.005)
+#filter = SavitzkyGolayFilter(window_size=20, polyorder=0)
+filter = MedianFilter(window_size=20)
 distance_estimator = LogdistancePathLossModel(n=2)
 
 pipeline.add_module(rssi_collector)
-pipeline.add_module(mean_filter)
+pipeline.add_module(filter)
 pipeline.add_module(distance_estimator)
 
 rssi_collector.start()
